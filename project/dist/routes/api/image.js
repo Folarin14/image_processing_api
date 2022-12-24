cd1fc445c9b39bd0cfd9409e8055dbc5c371d472
Folarin14 <pholalawal@yahoo.com>
2022-12-24 13:54:07 -0500

    moved images folder to project root

--- project/dist/routes/api/image.js
+++ project/dist/routes/api/image.js
@@ -2,31 +2,32 @@ import express from 'express';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import image_resize from '../../utils/processing.js';
-import checkFileExists from '../../utils/fs_utils.js';
+import { checkFileExists, getProjectRoot } from '../../utils/fs_utils.js';
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const rootDir = getProjectRoot(__filename, 3);
+// const __dirname = path.dirname(__filename);
 const image = express.Router();
 image.get('/', (req, res) => {
     const userQuery = req.query;
     const fileName = userQuery.filename;
     const imageFile = fileName + '.jpg';
     //check if file name exists
-    const fullPath = path.join(__dirname, 'images', `${imageFile}`);
+    const fullPath = path.join(rootDir, 'images', `${imageFile}`);
     if (checkFileExists(fullPath)) {
         const width = parseInt(userQuery.width) || undefined;
         const height = parseInt(userQuery.height) || undefined;
         if (userQuery.filename && !userQuery.width && !userQuery.height) {
-            res.sendFile(path.join(__dirname, 'images', `${imageFile}`));
+            res.sendFile(path.join(rootDir, 'images', `${imageFile}`));
         }
         else if (req.query.filename && (width || height)) {
             // before resizing, check if file already exists then serve it, else resize afresh
-            const queryImage = path.join(__dirname, 'images', 'resized', `${fileName}_${width}x${height}.jpg`);
+            const queryImage = path.join(rootDir, 'images', 'resized', `${fileName}_${width}x${height}.jpg`);
             if (checkFileExists(queryImage)) {
                 res.sendFile(queryImage);
             }
             else {
-                image_resize(path.join(__dirname, 'images', `${imageFile}`), path.join(__dirname, 'images', 'resized', `${fileName}_${width}x${height}.jpg`), width, height).then(() => {
-                    res.sendFile(path.join(__dirname, 'images', 'resized', `${fileName}_${width}x${height}.jpg`));
+                image_resize(path.join(rootDir, 'images', `${imageFile}`), path.join(rootDir, 'images', 'resized', `${fileName}_${width}x${height}.jpg`), width, height).then(() => {
+                    res.sendFile(path.join(rootDir, 'images', 'resized', `${fileName}_${width}x${height}.jpg`));
                 });
             }
         }
@@ -35,4 +36,4 @@ image.get('/', (req, res) => {
         res.send('Image API is active but image is not specified or does not exist. Verify image, then try again');
     }
 });
-export { image, __dirname };
+export { image, rootDir };
--- project/src/utils/fs_utils.ts
+++ project/src/utils/fs_utils.ts
@@ -1,8 +1,7 @@
-// Script to check if a file exists
-
-// Attribution: https://nodejs.org/api/fs.html#fsaccesspath-mode-callback
 
 import { accessSync, constants } from 'node:fs';
+import path from 'node:path';
+
 
 function checkFileExists(filePath: string): boolean {
 	try {
@@ -13,4 +12,15 @@ function checkFileExists(filePath: string): boolean {
 	}
 }
 
-export default checkFileExists;
+function getProjectRoot(currentFilePath: string, numberOfStepsToRoot: number): string {
+
+    //const __filename = fileURLToPath(import.meta.url);
+    const dirPath = path.dirname(currentFilePath)
+    const dirPathArray = dirPath.split(path.sep)
+    const projectDirectory = path.normalize(dirPathArray.slice(0, -numberOfStepsToRoot).join('\\'))
+
+    return projectDirectory
+
+}
+
+export {checkFileExists, getProjectRoot};
